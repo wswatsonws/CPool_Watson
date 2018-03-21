87aaae1f999935246385ccfaefd59a64cde61740
17150009 <17150009@bjtu.edu.cn>
2018-03-21 12:51:50 +0800

    10 commit

--- CPool_Day03/ex_07/my_put_nbr.c
+++ CPool_Day03/ex_07/my_put_nbr.c
@@ -1,35 +1,57 @@
 #include<stdio.h>
 #include<unistd.h>
-
+#include<stdlib.h>
 
 int my_putchar(int c)
 {
 	write(1,&c,1);
 	return 0;
 }
+
+int my_putstr(char const *str)
+{
+	int len=0;
+	char  const *n;
+	n = str;
+	while((*n++))
+	{
+		len++;
+	}
+	write(1,str,len);
+	return 0;
+}
+
 int my_put_nbr(int nb)
 {	
-	if(nb >= 10)
+	if(nb == -2147483648)
 	{
-		my_put_nbr(nb / 10);	
-		nb=nb % 10;
+		my_putstr("-2147483648");
 	}
-	if(nb < 10 && nb >= 0){
-		my_putchar(nb + '0');
-	}
-	if(nb < 0)
+	else
 	{
-		int k = -1*nb;
-		my_putchar('-');
-		if(k >= 10)
+		if(nb >= 10)
 		{
-			my_put_nbr(k / 10);	
-			k = k % 10;
+			my_put_nbr(nb / 10);	
+			nb=nb % 10;
 		}
-		if(k < 10 && k >= 0)
+		if(nb < 10 && nb >= 0)
 		{
-			my_putchar(k + '0');
-		}	
+			my_putchar(nb + '0');
+		}
+		if(nb < 0)
+		{
+			int k = -1 * nb;
+			my_putchar('-');
+			if(k >= 10)
+			{
+				my_put_nbr(k / 10);	
+				k = k % 10;
+			}
+			if(k < 10 && k >= 0)
+			{
+				my_putchar(k + '0');
+			}	
+		}
 	}
 	return 0;
 }
--- CPool_Day03/tests/tests-my_put_nbr.c
+++ CPool_Day03/tests/tests-my_put_nbr.c
@@ -6,6 +6,6 @@ int main(void)
 {
 	my_put_nbr(42);
 	my_put_nbr(0);
-	my_put_nbr(-2147483647);
+	my_put_nbr(-2147483648);
 	return 0;
 }
