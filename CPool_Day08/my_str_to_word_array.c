e64425e69391d6b94b00d67bd57d32677a89e787
17150009 <17150009@bjtu.edu.cn>
2018-03-22 23:39:58 +0800

    11 commit

--- CPool_Day08/my_str_to_word_array.c
+++ CPool_Day08/my_str_to_word_array.c
@@ -6,35 +6,42 @@ int my_putstr(char const *str);
 
 int my_strlen(char const *str);
 
-char **my_str_to_word_array(char const *str)
+int find_word(char const *str)
 {
 	int i = 0;
-	int j = 0;
-	int k = 0;
-	int x = 0;
-	char **dest;
-	dest = malloc(sizeof(char*) * (my_strlen(str) + 1));
-	dest[0] = malloc(sizeof(char) * (my_strlen(str) + 1));
-	while(str[i] != '\0') 
-	{
-		if((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') ||  (str[i] >= '0' && str[i] <= '9')) 
-		{
-			x = 1;
-			dest[j][k] = str[i];
-			k++;
-		} 
-		else
-		{
-			if(x == 1) 
-			{
-				dest[j + 1] = malloc(sizeof(char) * (my_strlen(str) + 1));
-				j++;
-				k = 0;
-				x = 0;
-			}
-		} 		
+	int nb = 0;
+	while (str[i] != '\0')
+	 {
+		while ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') ||  (str[i] >= '0' && str[i] <= '9'))
+			i++;
+		nb++;
 		i++;
 	}
-	return dest;
+	return (nb);
 }
 
+char **my_str_to_word_array(char const *str)
+{
+	int nb = find_word(str);
+	char **array = malloc(sizeof(char *) * (nb + 1));
+	int i = 0;
+	int character;
+	int key = 0;
+	
+	while (str[i] != '\0') 
+	{
+		array[key] = malloc(sizeof(char **) * my_strlen(str));
+		while(!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') ||  (str[i] >= '0' && str[i] <= '9')))
+			i++;
+		character = 0;
+		while((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') ||  (str[i] >= '0' && str[i] <= '9')) 
+		{
+			array[key][character] = str[i];
+			i++;
+			character++;
+		}
+		key++;
+	}
+	array[key] = 0;
+	return array;
+}
