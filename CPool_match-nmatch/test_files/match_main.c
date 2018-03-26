936a84463e92030a2777d82c9e3fc3a6d5e12e67
Watson <you@example.com>
2018-03-26 09:46:41 +0800

    10 commit

--- CPool_match-nmatch/test_files/match_main.c
+++ CPool_match-nmatch/test_files/match_main.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
 #include "../include/my.h"
 int match(char const *s1 , char const *s2 ) ;
-int main(int argc, const char * argv[]) 
-{
-	my_put_nbr(match(argv[1], argv[2]));
-	my_putchar('\n');
-	return 0;
-}
+
